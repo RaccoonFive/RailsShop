a9cd948d6e718017c2e266268821966d86b41518
Keven Parent <1438062@pmminfo45.local>
2016-08-26 13:49:26 -0300

    Added item form

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,21 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @user = User.find(session[:user][0]['id'])
+    @item.user = @user
+    if @item.save
+      redirect_to root_path
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :content)
+  end
 end
