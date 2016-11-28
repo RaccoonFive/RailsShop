8cb8348079705c62dcbbe6f99b4ffd646f9ef414
Keven Parent <lenigma444@hotmail.com>
2016-11-27 21:19:59 -0500

    Added login error message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,19 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def index
+    redirect_to new_session_path
+  end
+
   def create
     @user = User.where(username: params[:user][:username], password: params[:user][:password])
     if(!@user.blank?)
       session[:user] = @user
+      redirect_to root_path
+    else
+      @error = "There was an error logging you in!"
+      render :template => "sessions/error"
     end
-    redirect_to root_path
   end
 
   def logout
